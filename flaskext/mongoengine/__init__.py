5b236bc6603d5a99a54a20d07e13be073dbcd72a
Alistair Roche <roche.a@gmail.com>
2011-05-18 16:50:31 +0100

    Created ListFieldPagination

--- flaskext/mongoengine/__init__.py
+++ flaskext/mongoengine/__init__.py
@@ -91,7 +91,7 @@ class Pagination(object):
 
     def prev(self, error_out=False):
         """Returns a :class:`Pagination` object for the previous page."""
-        assert self.iterable is not None, 'a query object is required ' \
+        assert self.iterable is not None, 'an object is required ' \
                                        'for this method to work'
         return self.__class__(self.iterable, self.page - 1, self.per_page)
 
@@ -107,7 +107,7 @@ class Pagination(object):
 
     def next(self, error_out=False):
         """Returns a :class:`Pagination` object for the next page."""
-        assert self.iterable is not None, 'a query object is required ' \
+        assert self.iterable is not None, 'an object is required ' \
                                        'for this method to work'
         return self.__class__(self.iterable, self.page + 1, self.per_page)
 
@@ -156,3 +156,54 @@ class Pagination(object):
                     yield None
                 yield num
                 last = num
+
+
+class ListFieldPagination(object):
+
+    def __init__(self, queryset, doc_id, field_name, page, per_page,
+                 total=None):
+        """Allows an array within a document to be paginated.
+
+        Queryset must contain the document which has the array we're
+        paginating, and doc_id should be it's _id.
+        Field name is the name of the array we're paginating.
+        Page and per_page work just like in Pagination.
+        Total is an argument because it can be computed more efficiently
+        elsewhere, but we still use array.length as a fallback.
+        """
+        if page < 1:
+            abort(404)
+
+        self.page = page
+        self.per_page = per_page
+
+        self.queryset = queryset
+        self.doc_id = doc_id
+        self.field_name = field_name
+
+        start_index = (page - 1) * per_page
+        end_index = page * per_page
+
+        self.items = queryset.get(id=doc_id
+            ).fields(messages={"$slice": [start_index, end_index]}
+            ).messages
+
+        if not total:
+            total = len(self.items)
+
+        if not self.items and page != 1:
+            abort(404)
+
+    def prev(self, error_out=False):
+        """Returns a :class:`Pagination` object for the previous page."""
+        assert self.items is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return self.__class__(self.queryset, self.doc_id, self.field_name,
+            self.page - 1, self.per_page, self.total)
+
+    def next(self, error_out=False):
+        """Returns a :class:`Pagination` object for the next page."""
+        assert self.iterable is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return self.__class__(self.queryset, self.doc_id, self.field_name,
+            self.page + 1, self.per_page, self.total)
