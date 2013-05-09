d9563ed8b8b0858bb8bcaeacda4f4ef0463e4716
Ross Lawley <ross.lawley@gmail.com>
2013-05-09 13:19:24 +0000

    RC1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except:
 
 setup(
     name='flask-mongoengine',
-    version='0.6',
+    version='0.7.0RC1',
     url='https://github.com/mongoengine/flask-mongoengine',
     license='BSD',
     author='Ross Lawley',
