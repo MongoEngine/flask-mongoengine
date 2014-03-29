b3547546ea7805b1f12c2da656bb4c9bd5190c37
Michael Dougherty <maackle.d@gmail.com>
2014-03-29 14:22:20 -0700

    fixed python 3 issue with map() iterator

--- flask_mongoengine/wtf/orm.py
+++ flask_mongoengine/wtf/orm.py
@@ -230,7 +230,7 @@ def model_fields(model, only=None, exclude=None, field_args=None, converter=None
         names = ((k, v.creation_counter) for k, v in model._fields.items())
     else:
         names = ((k, v.creation_counter) for k, v in model._fields.iteritems())
-    field_names = map(itemgetter(0), sorted(names, key=itemgetter(1)))
+    field_names = list(map(itemgetter(0), sorted(names, key=itemgetter(1))))
 
     if only:
         field_names = set((x for x in only if x in set(field_names)))
