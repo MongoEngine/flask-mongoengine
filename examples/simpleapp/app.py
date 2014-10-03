70edf88a26bef09adb97b166563cc8189f01bd95
Andrew Elkins <andrewelkins@users.noreply.github.com>
2014-10-02 18:06:00 -0700

    Update app.py

--- examples/simpleapp/app.py
+++ examples/simpleapp/app.py
@@ -48,7 +48,6 @@ def index():
     Todo(title="Simple todo A ПЫЩЬ!", text="12345678910").save()  # Insert
     Todo(title="Simple todo B", text="12345678910").save()  # Insert
     Todo.objects(title__contains="B").update(set__text="Hello world")  # Update
-    todos = list(Todo.objects[:10])
     todos = Todo.objects.all()
     return flask.render_template('index.html', todos=todos)
 
