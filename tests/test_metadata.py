e66f0584270ed1335c4f76e467d6aa7f086935c8
losintikfos <brightdadson@gmail.com>
2015-11-05 14:34:36 +0000

    - Resolve Python2 assertIsNotNone issue

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -9,12 +9,12 @@ class MetaDataTestCase(FlaskMongoEngineTestCase):
         self.base_dir = os.path.split(os.path.dirname(__file__))[0]
 
     def test_metadata(self):
-        self.assertIsNotNone(self.base_dir)
+        self.assertTrue(self.base_dir is not None)
 
         metadata_script = os.path.join(self.base_dir, "flask_mongoengine", "metadata.py")
-        self.assertIsNotNone(metadata_script)
+        self.assertTrue(metadata_script is not None)
 
-        self.assertIsNone(self.metadata)
+        self.assertTrue(self.metadata is None)
         if sys.version_info >= (3, 0):
             from importlib.machinery import SourceFileLoader
             self.metadata = SourceFileLoader("metadata", metadata_script).load_module()
@@ -22,8 +22,8 @@ class MetaDataTestCase(FlaskMongoEngineTestCase):
             import imp
             self.metadata = imp.load_source('metadata', metadata_script)
 
-        self.assertIsNotNone(self.metadata)
-        self.assertIsNotNone(self.metadata.__version__)
+        self.assertTrue(self.metadata is not None)
+        self.assertTrue(self.metadata.__version__ is not None)
 
         # Ensures change in version is legitimate
         msg = "Version do not match value in metadata.py `VERSION`"
