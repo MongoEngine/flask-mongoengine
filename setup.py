da443220ccc1dcc1f7e3de4bbeb7ecdd10f65e19
losintikfos <brightdadson@gmail.com>
2015-11-07 15:05:41 +0000

    - Removed duplicate topic classifier for setup.py

--- setup.py
+++ setup.py
@@ -76,6 +76,5 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
