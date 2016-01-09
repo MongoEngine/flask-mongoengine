1da628fb64b035fdde94e4f19e5ecc63745a01c3
Bright Dadson <brightdadson@gmail.com>
2016-01-09 16:30:54 +0000

    Added test coverage for promoted version 0.7.5

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -23,7 +23,7 @@ class MetaDataTestCase(FlaskMongoEngineTestCase):
         # Ensures change in version is legitimate
         msg = "Version do not match value in metadata.py `VERSION`"
 
-        self.assertEqual("0.7.4", self.metadata.__version__, msg)
+        self.assertEqual("0.7.5", self.metadata.__version__, msg)
 
 if __name__ == '__main__':
     unittest.main()
