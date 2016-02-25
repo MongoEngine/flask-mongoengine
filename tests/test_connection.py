ccafaaaddd1db23078b8dd12882023d0547a2557
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2016-02-25 17:30:45 +0100

    mongomock tests: Python2.6 compatibility

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -6,32 +6,31 @@ from distutils.version import StrictVersion
 from flask.ext.mongoengine import MongoEngine
 from tests import FlaskMongoEngineTestCase
 
+
 class ConnectionTestCase(FlaskMongoEngineTestCase):
 
-	def test_ignore_parse_uri_if_testing_true_and_uses_mongomock_schema(self):
-		self.app.config['TESTING'] = True
-		self.app.config['MONGODB_ALIAS'] = 'unittest'
-		self.app.config['MONGODB_HOST'] = 'mongomock://localhost'
+    def test_ignore_parse_uri_if_testing_true_and_uses_mongomock_schema(self):
+        self.app.config['TESTING'] = True
+        self.app.config['MONGODB_ALIAS'] = 'unittest'
+        self.app.config['MONGODB_HOST'] = 'mongomock://localhost'
 
-		if StrictVersion(mongoengine.__version__) >= StrictVersion('0.10.6'):
-			db = MongoEngine(self.app)
-			self.assertTrue(isinstance(db.connection, mongomock.MongoClient))
+        if StrictVersion(mongoengine.__version__) >= StrictVersion('0.10.6'):
+            db = MongoEngine(self.app)
+            self.assertTrue(isinstance(db.connection, mongomock.MongoClient))
 
-	def test_parse_uri_if_testing_true_and_not_uses_mongomock_schema(self):
-		self.app.config['TESTING'] = True
-		self.app.config['MONGODB_ALIAS'] = 'unittest'
-		self.app.config['MONGODB_HOST'] = 'mongo://localhost'
+    def test_parse_uri_if_testing_true_and_not_uses_mongomock_schema(self):
+        self.app.config['TESTING'] = True
+        self.app.config['MONGODB_ALIAS'] = 'unittest'
+        self.app.config['MONGODB_HOST'] = 'mongo://localhost'
 
-		with self.assertRaises(InvalidURI):
-			db = MongoEngine(self.app)
+        self.assertRaises(InvalidURI, MongoEngine, self.app)
 
-	def test_parse_uri_if_testing_is_not_true(self):
-		self.app.config['TESTING'] = False
-		self.app.config['MONGODB_ALIAS'] = 'unittest'
-		self.app.config['MONGODB_HOST'] = 'mongomock://localhost'
-        
-		with self.assertRaises(InvalidURI):
-			db = MongoEngine(self.app)
+    def test_parse_uri_if_testing_not_true(self):
+        self.app.config['TESTING'] = False
+        self.app.config['MONGODB_ALIAS'] = 'unittest'
+        self.app.config['MONGODB_HOST'] = 'mongomock://localhost'
+
+        self.assertRaises(InvalidURI, MongoEngine, self.app)
 
 if __name__ == '__main__':
     unittest.main()
