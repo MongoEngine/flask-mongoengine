f8afcdc6b57b222eaebe729fb472f5cd008baa90
Thanathip Limna <boatkrap@gmail.com>
2020-05-11 11:17:35 +0700

    add _Auto as formdata in constructor for flask-wtf compatition

--- flask_mongoengine/wtf/models.py
+++ flask_mongoengine/wtf/models.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-
+from flask_wtf.form import _Auto
 
 class ModelForm(FlaskForm):
     """A WTForms mongoengine model form"""
 
-    def __init__(self, formdata=None, **kwargs):
+    def __init__(self, formdata=_Auto, **kwargs):
         self.instance = kwargs.pop("instance", None) or kwargs.get("obj")
         if self.instance and not formdata:
             kwargs["obj"] = self.instance
