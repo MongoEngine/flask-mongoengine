4bcd64f90845cb3c48766d445659299ff37e5df0
Andrey Shpak <ashpak@ashpak.ru>
2020-06-08 05:44:16 +0300

    Final migration of test_connection tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,9 +1,13 @@
 import mongoengine
+import pymongo
 import pytest
 from mongoengine.connection import ConnectionFailure
+from mongoengine.context_managers import switch_db
 from pymongo.database import Database
-from pymongo.mongo_client import MongoClient
 from pymongo.errors import InvalidURI
+from pymongo.mongo_client import MongoClient
+from pymongo.read_preferences import ReadPreference
+
 from flask_mongoengine import MongoEngine, current_mongoengine_instance
 
 
@@ -195,3 +199,130 @@ def test_connection__should_raise__if_uri_not_properly_formatted(app, config_ext
         str(error.value)
         == "Invalid URI scheme: URI must begin with 'mongodb://' or 'mongodb+srv://'"
     )
+
+
+def test_connection__should_accept_host_as_list(app):
+    """Make sure MONGODB_HOST can be a list hosts."""
+    db = MongoEngine()
+    app.config["MONGODB_SETTINGS"] = {
+        "ALIAS": "host_list",
+        "HOST": ["localhost:27017"],
+        "DB": "flask_mongoengine_list_test_db",
+    }
+    db.init_app(app)
+
+    connection = mongoengine.get_connection("host_list")
+    mongo_engine_db = mongoengine.get_db("host_list")
+    assert isinstance(mongo_engine_db, Database)
+    assert isinstance(connection, MongoClient)
+    assert mongo_engine_db.name == "flask_mongoengine_list_test_db"
+    assert connection.HOST == "localhost"
+    assert connection.PORT == 27017
+
+
+def test_multiple_connections(app):
+    """Make sure establishing multiple connections to a standalone
+    MongoDB and switching between them works.
+    """
+    db = MongoEngine()
+    app.config["MONGODB_SETTINGS"] = [
+        {
+            "ALIAS": "default",
+            "DB": "flask_mongoengine_test_db_1",
+            "HOST": "localhost",
+            "PORT": 27017,
+        },
+        {
+            "ALIAS": "alternative",
+            "DB": "flask_mongoengine_test_db_2",
+            "HOST": "localhost",
+            "PORT": 27017,
+        },
+    ]
+
+    class Todo(db.Document):
+        title = db.StringField(max_length=60)
+
+    db.init_app(app)
+    # Drop default collection from init
+    Todo.drop_collection()
+    Todo.meta = {"db_alias": "alternative"}
+    # Drop 'alternative' collection initiated early.
+    Todo.drop_collection()
+
+    # Make sure init correct and both databases are clean
+    with switch_db(Todo, "default") as Todo:
+        doc = Todo.objects().first()
+        assert doc is None
+
+    with switch_db(Todo, "alternative") as Todo:
+        doc = Todo.objects().first()
+        assert doc is None
+
+    # Test saving a doc via the default connection
+    with switch_db(Todo, "default") as Todo:
+        todo = Todo()
+        todo.text = "Sample"
+        todo.title = "Testing"
+        todo.done = True
+        s_todo = todo.save()
+
+        f_to = Todo.objects().first()
+        assert s_todo.title == f_to.title
+
+    # Make sure the doc still doesn't exist in the alternative db
+    with switch_db(Todo, "alternative") as Todo:
+        doc = Todo.objects().first()
+        assert doc is None
+
+    # Make sure switching back to the default connection shows the doc
+    with switch_db(Todo, "default") as Todo:
+        doc = Todo.objects().first()
+        assert doc is not None
+
+
+def test_ingnored_mongodb_prefix_config(app):
+    """Config starting by MONGODB_ but not used by flask-mongoengine
+    should be ignored.
+    """
+    db = MongoEngine()
+    app.config[
+        "MONGODB_HOST"
+    ] = "mongodb://localhost:27017/flask_mongoengine_test_db_prod"
+    # Invalid host, should trigger exception if used
+    app.config["MONGODB_TEST_HOST"] = "dummy://localhost:27017/test"
+    db.init_app(app)
+
+    connection = mongoengine.get_connection()
+    mongo_engine_db = mongoengine.get_db()
+    assert isinstance(mongo_engine_db, Database)
+    assert isinstance(connection, MongoClient)
+    assert mongo_engine_db.name == "flask_mongoengine_test_db_prod"
+    assert connection.HOST == "localhost"
+    assert connection.PORT == 27017
+
+
+def test_connection_kwargs(app):
+    """Make sure additional connection kwargs work."""
+
+    # Figure out whether to use "MAX_POOL_SIZE" or "MAXPOOLSIZE" based
+    # on PyMongo version (former was changed to the latter as described
+    # in https://jira.mongodb.org/browse/PYTHON-854)
+    # TODO remove once PyMongo < 3.0 support is dropped
+    if pymongo.version_tuple[0] >= 3:
+        MAX_POOL_SIZE_KEY = "MAXPOOLSIZE"
+    else:
+        MAX_POOL_SIZE_KEY = "MAX_POOL_SIZE"
+
+    app.config["MONGODB_SETTINGS"] = {
+        "ALIAS": "tz_aware_true",
+        "DB": "flask_mongoengine_testing_tz_aware",
+        "TZ_AWARE": True,
+        "READ_PREFERENCE": ReadPreference.SECONDARY,
+        MAX_POOL_SIZE_KEY: 10,
+    }
+    db = MongoEngine(app)
+
+    assert db.connection.codec_options.tz_aware
+    assert db.connection.max_pool_size == 10
+    assert db.connection.read_preference == ReadPreference.SECONDARY
