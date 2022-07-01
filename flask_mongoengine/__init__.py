725d725b364a966c5ff9a2921cd2dfa5c2a8762b
Andrey Shpak <ashpak@ashpak.ru>
2022-07-01 23:30:40 +0300

    Extend first_or_404 and get_or_404 with _message_404 key + related docstrings update

--- flask_mongoengine/__init__.py
+++ flask_mongoengine/__init__.py
@@ -145,22 +145,36 @@ class MongoEngine(object):
 
 
 class BaseQuerySet(QuerySet):
-    """Mongoengine's queryset extended with handy extras."""
+    """Extends :class:`mongoengine.QuerySet` class with handly methods."""
 
-    def get_or_404(self, *args, message_404=None, **kwargs):
+    def _abort_404(self, _message_404):
+        """Returns 404 error with message, if message provided.
+
+        :param _message_404: Message for 404 comment
         """
-        Get a document and raise a 404 Not Found error if it doesn't
-        exist.
+        abort(404, _message_404) if _message_404 else abort(404)
+
+    def get_or_404(self, *args, _message_404=None, **kwargs):
+        """Get a document and raise a 404 Not Found error if it doesn't exist.
+
+        :param _message_404: Message for 404 comment, not forwarded to
+            :func:`~QuerySet.get`
+        :param args: args list, silently forwarded to :func:`~QuerySet.get`
+        :param kwargs: keywords arguments, silently forwarded to :func:`~QuerySet.get`
         """
         try:
             return self.get(*args, **kwargs)
         except DoesNotExist:
-            abort(404, message_404) if message_404 else abort(404)
+            self._abort_404(_message_404)
 
-    def first_or_404(self, message=None):
-        """Same as get_or_404, but uses .first, not .get."""
-        obj = self.first()
-        return obj if obj else abort(404, message) if message else abort(404)
+    def first_or_404(self, _message_404=None):
+        """Same as :func:`~BaseQuerySet.get_or_404`, but uses
+        :func:`~BaseQuerySet.first`, not :func:`~QuerySet.get`.
+
+        :param _message_404: Message for 404 comment, not forwarded to
+            :func:`~QuerySet.get`
+        """
+        return self.first() or self._abort_404(_message_404)
 
     def paginate(self, page, per_page, **kwargs):
         """
