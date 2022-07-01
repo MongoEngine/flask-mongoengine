94927cb179babaed43a37c0704728fe4e539a259
Andrey Shpak <ashpak@ashpak.ru>
2022-07-01 20:31:36 +0300

    Move bigger app to example_app

--- example_app/app.py
+++ example_app/app.py
@@ -1,11 +1,7 @@
-import os
-import sys
-
 import flask
-
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), "../../")))
-
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db
+from views import index, pagination
 
 app = flask.Flask(__name__)
 app.config.from_object(__name__)
@@ -24,13 +20,10 @@ app.config["DEBUG_TB_PANELS"] = (
 )
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
-from models import db
-
 db.init_app(app)
 
 DebugToolbarExtension(app)
 
-from views import index, pagination
 
 app.add_url_rule("/", view_func=index)
 app.add_url_rule("/pagination", view_func=pagination)
