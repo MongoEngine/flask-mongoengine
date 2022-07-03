d28259a03e570d5bb103bbc5c3871ad49e1345fa
Andrey Shpak <ashpak@ashpak.ru>
2022-07-03 18:46:40 +0300

    Add all possible panels to example application

--- example_app/app.py
+++ example_app/app.py
@@ -13,12 +13,16 @@ app.config["TESTING"] = True
 app.config["SECRET_KEY"] = "flask+mongoengine=<3"
 app.debug = True
 app.config["DEBUG_TB_PANELS"] = (
-    "flask_debugtoolbar.panels.versions.VersionDebugPanel",
-    "flask_debugtoolbar.panels.timer.TimerDebugPanel",
+    "flask_debugtoolbar.panels.config_vars.ConfigVarsDebugPanel",
+    "flask_debugtoolbar.panels.g.GDebugPanel",
     "flask_debugtoolbar.panels.headers.HeaderDebugPanel",
-    "flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel",
-    "flask_debugtoolbar.panels.template.TemplateDebugPanel",
     "flask_debugtoolbar.panels.logger.LoggingPanel",
+    "flask_debugtoolbar.panels.profiler.ProfilerDebugPanel",
+    "flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel",
+    "flask_debugtoolbar.panels.route_list.RouteListDebugPanel",
+    "flask_debugtoolbar.panels.template.TemplateDebugPanel",
+    "flask_debugtoolbar.panels.timer.TimerDebugPanel",
+    "flask_debugtoolbar.panels.versions.VersionDebugPanel",
     "flask_mongoengine.panels.MongoDebugPanel",
 )
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
